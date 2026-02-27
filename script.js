9e8231d90f6a14af1f07ccad2c95cc0f4f1bb9b9
Rogerio Luiz <rogeriovnuzpizolato@gmail.com>
2026-02-27 03:28:10 -0300

    funcionalidade do botao limpar inplementada com sucesso

--- script.js
+++ script.js
@@ -1,8 +1,13 @@
 function adicionarAtividade() {
   let input = document.getElementById("atividade");
-  let atividade = input.value;
+  let texto= input.value.trim();
 
-  if (atividade.trim() !== "") {
+  if (texto !== "") {
+    let tarefa= {
+      texto:input.value,
+      concluida:false
+    };
+
     let li = document.createElement("li");
 
     let checkbox = document.createElement("input");
@@ -10,41 +15,57 @@ function adicionarAtividade() {
 
     li.appendChild(checkbox);
 
-    li.appendChild(document.createTextNode("" + atividade));
+    li.appendChild(document.createTextNode("" + tarefa.texto));
 
     let lista = document.getElementById("listaDeAtividades");
 
     lista.appendChild(li);
 
-    salvarLocalStorage(atividade);
+    salvarLocalStorage(tarefa);
 
     input.value = "";
   }
 }
 
-function salvarLocalStorage(atividade) {
+function salvarLocalStorage(tarefa) {
   let tarefas = JSON.parse(localStorage.getItem('tarefas')) || [];
 
-  tarefas.push(atividade);
+  tarefas.push(tarefa);
 
-  localStorage.setItem("tarefas", JSON.stringify(tarefas  ));
+  localStorage.setItem("tarefas", JSON.stringify(tarefas));
 }
 
 document.addEventListener('DOMContentLoaded',function () {
   let tarefas = JSON.parse(localStorage.getItem("tarefas")) || [];
 
-  tarefas.forEach(function (atividade) {
+  tarefas.forEach(function (tarefa,index) {
+
     let li = document.createElement("li");
 
     let checkbox = document.createElement("input");
     checkbox.type = "checkbox";
+    checkbox.checked= tarefa.concluida;
+
+    // quando desnarcar/marcar, salva novamente
+    checkbox.addEventListener('change',function(){
+      tarefas[index].concluida=checkbox.checked;
+      localStorage.setItem('tarefas',JSON.stringify(tarefas));
+    });
 
     li.appendChild(checkbox);
 
-    li.appendChild(document.createTextNode("" + atividade));
+    li.appendChild(document.createTextNode("" + tarefa.texto));
 
     let lista = document.getElementById("listaDeAtividades");
 
     lista.appendChild(li);
   });
 });
+
+function limparAtividades(){
+  if(confirm('tem certeza que deseja apagar as atividades salvas?')){
+    localStorage.removeItem('tarefas');
+    let lista= document.getElementById('listaDeAtividades');
+    lista.innerHTML='';
+  }
+}
