e1a09387a0c3a7f45c0919b3a53f36d75a1d4cb0
Rogerio Luiz <rogeriovnuzpizolato@gmail.com>
2026-03-25 17:15:04 -0300

    feita nova estrutura de dados do javascript do projeto

---
+++ js/date.js
@@ -0,0 +1,25 @@
+export function verificarNovoDia() {
+  const hoje = new Date().toISOString().split("T")[0];
+  const ultimaData = localStorage.getItem("ultimaData");
+
+  if (!ultimaData) {
+    localStorage.setItem("ultimaData", hoje);
+    return;
+  }
+
+  if (hoje !== ultimaData) {
+    console.log("Novo Dia Detectado");
+    localStorage.removeItem("tarefas");
+    localStorage.setItem("ultimaData", hoje);
+  }
+}
+
+export function verificarVersao() {
+  const versaoAtual = "1.2";
+  const versaoSalva = localStorage.getItem("versao");
+
+  if (versaoSalva !== versaoAtual) {
+    localStorage.removeItem("dadosRotina");
+    localStorage.setItem("versao", versaoAtual);
+  }
+}
