e8945ea6b4e99f89173fab71cf8830b33384ef1c
Rogerio Luiz <rogeriovnuzpizolato@gmail.com>
2026-06-21 01:44:08 -0300

    feat: termino da funcao para mostrar data e hora no dom

--- script.js
+++ script.js
@@ -1,15 +1,22 @@
 const h1Date= document.getElementById("date");
+const h2Hora= document.getElementById("horas");
 
-function mostrarHora(){
+function mostrarData(){
    const hoje= new Date()
 
    const dia= hoje.getDate();
    const mes= new Intl.DateTimeFormat("pt-br", {month: "long"}).format(hoje);
+   const ano= hoje.getFullYear();
 
    hoje.toLocaleString("pt-br");
+   h1Date.innerHTML= `${dia} de ${mes}  ${ano}`
 
-   h1Date.innerHTML= `${dia} de ${mes}`
-//    h1Date.append(dia,mes)
+    //    pegando a hora 
+    const hora= String(hoje.getHours()).padStart(2,"0");
+    const minutos= String(hoje.getMinutes()).padStart(2,"0");
+
+    h2Hora.textContent= `${hora}:${minutos}`;
+    setInterval(mostrarData, 1000)
 }
-mostrarHora()
+mostrarData()
 
